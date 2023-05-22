d00fae8ff3805716fa8466996d0dc7684c8c7aad
Michal Przadka <przadka@gmail.com>
2023-05-22 10:41:08 +0200

    Refactored tests using fixtures

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,24 +1,21 @@
+import pytest
 from app import create_app
 
-def test_index():
+@pytest.fixture(scope='module')
+def test_client():
     app = create_app()
-    client = app.test_client()
+    with app.test_client() as client:
+        yield client
 
-    response = client.get('/')
+def test_index(test_client):
+    response = test_client.get('/')
     assert response.status_code == 200
     assert b"Welcome to my app!" in response.data
 
-
-def test_authorize_redirect():
-    app = create_app()
-    client = app.test_client()
-
-    response = client.get('/authorize', follow_redirects=False)
+def test_authorize_redirect(test_client):
+    response = test_client.get('/authorize', follow_redirects=False)
     assert response.status_code == 302
 
-def test_oauth2callback_without_code():
-    app = create_app()
-    client = app.test_client()
-
-    response = client.get('/oauth2callback')
+def test_oauth2callback_without_code(test_client):
+    response = test_client.get('/oauth2callback')
     assert response.status_code == 400
