8836b7209e3a319955b98980cb0fc25c0f029b52
Michal Przadka <przadka@gmail.com>
2023-05-22 10:31:32 +0200

    Tests are now working

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,31 +1,26 @@
 import pytest
-import os
+
 from app import create_app
 
-@pytest.fixture
-def app():
-    app = create_app({
-        'TESTING': True,
-        'CLIENT_ID': os.getenv('CLIENT_ID'),
-        'CLIENT_SECRET': os.getenv('CLIENT_SECRET'),
-    })
-    yield app
+def test_index():
+    app = create_app()
+    client = app.test_client()
 
-@pytest.fixture
-def client(app):
-    return app.test_client()
-
-def test_index(client):
     response = client.get('/')
     assert response.status_code == 200
-    assert b'Welcome to my app!' in response.data
+    assert b"Welcome to my app!" in response.data
 
-def test_authorize_redirect(client):
+
+def test_authorize_redirect():
+    app = create_app()
+    client = app.test_client()
+
     response = client.get('/authorize', follow_redirects=False)
     assert response.status_code == 302
 
-def test_oauth2callback_without_code(client):
+def test_oauth2callback_without_code():
+    app = create_app()
+    client = app.test_client()
+
     response = client.get('/oauth2callback')
     assert response.status_code == 400
-
-# Here, you may add other tests for the remaining routes and functions
