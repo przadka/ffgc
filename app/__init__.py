63ce6741a427a4ca9d2d010319f73b3679aa640c
Michal Przadka <przadka@gmail.com>
2023-05-22 10:40:59 +0200

    Refactored create app function

--- app/__init__.py
+++ app/__init__.py
@@ -1,27 +1,23 @@
 import os
 from flask import Flask
 
-def create_app(config_dict=None):
+def create_app():
 
-    if config_dict is None:
-        app = Flask(__name__)
-        app.secret_key = os.getenv('SECRET_KEY')
+    app = Flask(__name__)
+    app.secret_key = os.getenv('SECRET_KEY')
 
-        # The client ID and client secret from your OAuth 2.0 credentials
-        app.config['CLIENT_ID'] = os.getenv('CLIENT_ID')
-        app.config['CLIENT_SECRET'] = os.getenv('CLIENT_SECRET')
+    # The client ID and client secret from your OAuth 2.0 credentials
+    app.config['CLIENT_ID'] = os.getenv('CLIENT_ID')
+    app.config['CLIENT_SECRET'] = os.getenv('CLIENT_SECRET')
 
-        # Ensure the instance folder exists
-        try:
-            os.makedirs(app.instance_path)
-        except OSError:
-            pass
+    # Ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
 
-        from . import routes
-        app.register_blueprint(routes.bp)
-    else:
-        app = Flask(__name__)
-        app.config.update(config_dict)
+    from . import routes
+    app.register_blueprint(routes.bp)
 
     return app
 
