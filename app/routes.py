a423733014d051f751631ada0a237af2c20bdb64
Michał Parkoła <michal.parkola@gmail.com>
2023-06-05 01:29:50 +0200

    New route /free lists available calendar slots in the next 7 days

--- app/routes.py
+++ app/routes.py
@@ -76,3 +76,60 @@ def calendar():
             eventDate = event['start'].get('dateTime', event['start'].get('date'))[:10]
             eventList += '<li>' + eventDate + ' - ' + event['summary'] + '</li>'
         return 'Upcoming events:' + '<ul>' + eventList + '</ul>'
+
+@bp.route('/free')
+def free():
+    # Load credentials from the session.
+    credentials_dict = json.loads(session['credentials'])
+    credentials = Credentials.from_authorized_user_info(credentials_dict)
+
+    # Build the Google Calendar API service
+    service = build('calendar', 'v3', credentials=credentials)
+
+    # TODO Use local timezone.
+    start_time = datetime.datetime.utcnow()
+    end_time = start_time + datetime.timedelta(days=7)
+
+    # Call the Calendar API
+    print('Checking free/busy information for the coming 7 days.')   
+    calendar_id = 'primary' # Check the user's primary calendar
+    request = service.freebusy().query(body={
+        "timeMin": start_time.isoformat() + "Z",
+        "timeMax": end_time.isoformat() + "Z",
+        "items": [{"id": calendar_id}]
+    })
+    response = request.execute()
+    busy_slots = response['calendars'][calendar_id]['busy']
+
+    # List busy slots
+    # output_busy = "<ul>"
+    # for busy_slot in busy_slots:
+    #    start = datetime.datetime.fromisoformat(busy_slot['start'])
+    #    end = datetime.datetime.fromisoformat(busy_slot['end'])
+    #    output_busy += "<li>Busy from " + start.isoformat() + " to " + end.isoformat() + "</li>"
+    # output_busy += "</ul>"
+
+    # Retrieve the free time slots by finding the gaps between busy slots
+    free_slots = []
+    if len(busy_slots) == 0:
+        # If there are no busy slots, the entire time range is free
+        free_slots.append({
+            'start': start_time.isoformat(),
+            'end': end_time.isoformat()
+        })
+    else:
+        # Iterate over the busy slots to identify the free time intervals
+        for i in range(len(busy_slots) - 1):
+            start = busy_slots[i]['end']
+            end = busy_slots[i + 1]['start']
+            free_slots.append({
+                'start': start,
+                'end': end
+            })
+    # Print the free time slots
+    output_free = "<ul>"
+    for slot in free_slots:
+        output_free += f"<li>Free from {slot['start']} to {slot['end']}</li>"
+    output_free += "</ul>"
+
+    return output_free
